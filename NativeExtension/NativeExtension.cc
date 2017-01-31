340e9b267fea5a32dde1307a3f715b950bcd81d6
Jerome Etienne <jerome.etienne@gmail.com>
2017-01-31 14:13:33 +0000

    more work

--- NativeExtension/NativeExtension.cc
+++ NativeExtension/NativeExtension.cc
@@ -8,16 +8,22 @@ using v8::FunctionTemplate;
 NAN_MODULE_INIT(InitAll) {
         Nan::Set(target, Nan::New("nothing").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(nothing)).ToLocalChecked());
+
         Nan::Set(target, Nan::New("aString").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(aString)).ToLocalChecked());
+
         Nan::Set(target, Nan::New("aBoolean").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(aBoolean)).ToLocalChecked());
+
         Nan::Set(target, Nan::New("aNumber").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(aNumber)).ToLocalChecked());
+
         Nan::Set(target, Nan::New("anObject").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(anObject)).ToLocalChecked());
+
         Nan::Set(target, Nan::New("anArray").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(anArray)).ToLocalChecked());
+
         Nan::Set(target, Nan::New("callback").ToLocalChecked(),
         Nan::GetFunction(Nan::New<FunctionTemplate>(callback)).ToLocalChecked());
         
